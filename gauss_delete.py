9e78f560435afb5e05b63c5a443447b6ae207836
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-09-06 14:53:39 +0900

    feat: implement_gause_deletion and round error occured

--- gauss_delete.py
+++ gauss_delete.py
@@ -1,20 +1,27 @@
-A = [[1,-2],
-    [2, 3]]
-b = [[1],
-    [3]]
+A = [[1,1,1],
+    [2,-1,1],
+    [1,2,-1]]
+b = [[6],
+    [3],
+    [2]]
 
 def gauss_del(A, b):
     ex_A = [a+bb for a,bb in zip(A,b)]
 
     for col in range(len(A[0])):
         pivot = ex_A[col]
+        pivot_num = pivot[col]
+
         for row in range(len(A)):
             if row==col:
                 continue
             elif ex_A[row][col] != 0:
-                print(pivot)
-                print((ex_A[row][col]/pivot[col]))
-                row -= pivot * (ex_A[row][col]/pivot[col])
+                target = ex_A[row]
+                ratio = target[col]/pivot[col]
+                ex_A[row] = [t - p * ratio for t, p in zip(target, pivot)]
+
+        ex_A[col] = [p / pivot_num for p in pivot]
+            
     return ex_A 
 
 if __name__ == "__main__":
